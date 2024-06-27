56b08b63eb8f4fca4e71358544659471b4cb913d
John Rackles <john@rackles.io>
2024-06-27 11:10:30 +0200

    feat: add headline

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,7 +5,10 @@ import Front from "~/assets/front.png?jsx";
 
 export default component$(() => {
   return (
-    <div class="flex  items-center justify-center bg-white">
+    <div class="flex flex-col items-center justify-center bg-white">
+      <h1 class="py-2 text-4xl font-bold lg:text-6xl">
+        Welcome to Thomas Abstellkammer, very Whimsical
+      </h1>
       <div class="group max-h-[500px]">
         <Front class="max-h-full w-auto group-hover:hidden" />
         <Back class="hidden h-full max-h-full w-auto group-hover:block" />
